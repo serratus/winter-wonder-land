0cfce53db928a79def144713aa4f2115ddbfb6d9
Christoph Oberhofer <christoph@starshiphsa.com>
2021-11-15 08:53:40 +0100

    Added a few day-styles

--- components/CalendarDay.tsx
+++ components/CalendarDay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 import type { CalendarDay as CalendarDayData } from './useWithCalendarData';
 
@@ -8,57 +8,126 @@ type CalendarDayProps = {
 
 const colorForDay = new Array(24).fill(0).map((_, i) => `#039d03`);
 
-export const CalendarDay: React.FC<CalendarDayProps> = ({ day }) => (
-  <div
-    style={{
-      flex: 1 / 6,
-      display: 'flex',
-      flexDirection: 'column',
-      position: 'relative',
-      marginBottom: `${((1 / 20) * 100 * 1) / (2 * Math.cos(Math.PI / 6))}%`,
-      marginTop: `${((1 / 20) * 100 * 1) / (2 * Math.cos(Math.PI / 6))}%`,
-      marginRight: `${((1 / 80) * 100 * 1) / Math.cos(Math.PI / 6)}%`,
-      marginLeft: `${((1 / 80) * 100 * 1) / Math.cos(Math.PI / 6)}%`,
-    }}
-  >
+const outerBorderColor = '#141414';
+const innerBorderColor = 'rgb(14 126 14)';
+const defaultBackgroundColor = `#039d03`;
+
+// pass in css attributes
+type HexagonStyle = Pick<
+  CSSProperties,
+  | 'backgroundColor'
+  | 'color'
+  | 'fontSize'
+  | 'fontFamily'
+  | 'fontStyle'
+  | 'fontWeight'
+  | 'textDecoration'
+  | 'backgroundImage'
+  | 'borderStyle'
+  | 'borderWidth'
+  | 'borderColor'
+  | 'position'
+  | 'top'
+  | 'left'
+  | 'justifyContent'
+  | 'alignItems'
+  | 'cursor'
+  | 'padding'
+  | 'margin'
+  | 'transform'
+>;
+
+const stylesForDay: HexagonStyle[] = [
+  {
+    color: 'white',
+  },
+  {
+    fontSize: '2rem',
+  },
+  {
+    fontWeight: 'bold',
+    fontSize: '2rem',
+  },
+  {
+    fontStyle: 'italic',
+  },
+  {
+    textDecoration: 'underline',
+    fontSize: '2rem',
+  },
+  { backgroundColor: 'violet' },
+];
+
+export const CalendarDay: React.FC<CalendarDayProps> = ({ day }) => {
+  const backgroundColor =
+    stylesForDay[day.dayOfMonth - 1]?.backgroundColor ?? defaultBackgroundColor;
+  return (
     <div
       style={{
-        backgroundColor: colorForDay[day.dayOfMonth - 1],
-        aspectRatio: `${2 * Math.cos(Math.PI / 6)} / 1`,
+        flex: 1 / 6,
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderLeft: '2px solid black',
-        borderRight: '2px solid black',
+        flexDirection: 'column',
+        position: 'relative',
+        marginBottom: `${((1 / 20) * 100 * 1) / (2 * Math.cos(Math.PI / 6))}%`,
+        marginTop: `${((1 / 20) * 100 * 1) / (2 * Math.cos(Math.PI / 6))}%`,
+        marginRight: `${((1 / 80) * 100 * 1) / Math.cos(Math.PI / 6)}%`,
+        marginLeft: `${((1 / 80) * 100 * 1) / Math.cos(Math.PI / 6)}%`,
       }}
     >
       <div
         style={{
-          backgroundColor: colorForDay[day.dayOfMonth - 1],
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          aspectRatio: '1 / 1',
-          top: '-88%',
-          transform: 'rotate(-45deg) skew(15deg, 15deg) scale(0.56)',
-          borderTop: '3px solid black',
-          borderRight: '3px solid black',
+          backgroundColor,
+          aspectRatio: `${2 * Math.cos(Math.PI / 6)} / 1`,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderLeft: `2px solid ${outerBorderColor}`,
+          borderRight: `2px solid ${outerBorderColor}`,
         }}
-      ></div>
-      <div
-        style={{
-          backgroundColor: colorForDay[day.dayOfMonth - 1],
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          aspectRatio: '1 / 1',
-          top: '12%',
-          transform: 'rotate(-45deg) skew(15deg, 15deg) scale(0.56)',
-          borderBottom: '3px solid black',
-          borderLeft: '3px solid black',
-        }}
-      ></div>
-      <div style={{ zIndex: 100 }}>{day.dayOfMonth}</div>
+      >
+        <div
+          style={{
+            backgroundColor,
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            aspectRatio: '1 / 1',
+            top: '-87%',
+            transform: 'rotate(-45deg) skew(15deg, 15deg) scale(0.558)',
+            borderTop: `3px solid ${outerBorderColor}`,
+            borderRight: `3px solid ${outerBorderColor}`,
+          }}
+        ></div>
+        <div
+          style={{
+            backgroundColor,
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            aspectRatio: '1 / 1',
+            top: '13%',
+            transform: 'rotate(-45deg) skew(15deg, 15deg) scale(0.558)',
+            borderBottom: `3px solid ${outerBorderColor}`,
+            borderLeft: `3px solid ${outerBorderColor}`,
+          }}
+        ></div>
+        <div
+          style={{
+            zIndex: 100,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            // backgroundColor: 'white',
+            // width: '100%',
+            // aspectRatio: '1',
+            // margin: '20%',
+            borderRadius: '100%',
+            // border: `2px solid ${innerBorderColor}`,
+          }}
+        >
+          <span style={stylesForDay[day.dayOfMonth - 1]}>{day.dayOfMonth}</span>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
